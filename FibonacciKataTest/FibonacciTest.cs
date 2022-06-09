908e313ec314c780c028ca5e1a9679ec5bf7910c
Rufino Perez <ruperez@aidacanarias.com>
2022-06-09 09:27:04 +0200

    green tests

--- FibonacciKataTest/FibonacciTest.cs
+++ FibonacciKataTest/FibonacciTest.cs
@@ -15,6 +15,9 @@ namespace FibonacciKataTest
         [TestCase(2, 1)]
         [TestCase(3, 2)]
         [TestCase(4, 3)]
+        [TestCase(5, 5)]
+        [TestCase(6, 8)]
+        [TestCase(10, 55)]
         public void return_fibonacci_for_number(int number, int expectedResult)
         {
             int result = Fibonacci.GetNumber(number);
